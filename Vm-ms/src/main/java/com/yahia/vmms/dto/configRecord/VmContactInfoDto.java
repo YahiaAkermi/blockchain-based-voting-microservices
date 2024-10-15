1fdd402870a5ff95631930ffedb5aee30bddc2c7
yahia <ya.akermi@esi-sba.dz>
2024-10-15 12:58:54 +0200

    i've iplmeneted fetching config from github and then injecting them in all ms also i'm refreshing manually the config so configserver can retrieve new one

--- Vm-ms/src/main/java/com/yahia/vmms/dto/configRecord/VmContactInfoDto.java
+++ Vm-ms/src/main/java/com/yahia/vmms/dto/configRecord/VmContactInfoDto.java
@@ -1,12 +1,19 @@
 package com.yahia.vmms.dto.configRecord;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "voting-managment")
-public record VmContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class VmContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
 
 
--- voting-ms/src/main/java/com/yahia/votingms/dto/configRecord/VoteContactInfoDto.java
+++ voting-ms/src/main/java/com/yahia/votingms/dto/configRecord/VoteContactInfoDto.java
@@ -1,10 +1,16 @@
 package com.yahia.votingms.dto.configRecord;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "voting-ms")
-public record VoteContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class VoteContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
