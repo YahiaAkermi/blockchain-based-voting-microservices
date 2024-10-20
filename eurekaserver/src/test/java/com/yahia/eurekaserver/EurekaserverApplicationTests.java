d0055e3e37030ee63b300f862a9a9aed473d907b
yahia <ya.akermi@esi-sba.dz>
2024-10-20 18:33:22 +0200

    i've implemented service registery after adding eureka server which is another module that gonna take careof registering and de-registering

---
+++ eurekaserver/src/test/java/com/yahia/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahia.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
